4b6c191da164944d7ad93db795ecff55960f8eda
eddy49reg <akhnazarov1990@gmail.com>
2023-01-16 14:35:26 +0300

    finish redact boomerang

--- src/game-models/Boomerang.js
+++ src/game-models/Boomerang.js
@@ -6,22 +6,18 @@ class Boomerang {
   }
 
   fly() {
-    if ((this.pathOfTheMush = 'goright')) {
+    if (this.direction === 'right') {
       this.moveRight();
     } else {
       this.moveLeft();
     }
-    // this.moveRight();
-    // this.moveLeft();
   }
 
   moveLeft() {
-    // Идём влево.
     this.position -= 1;
   }
 
   moveRight() {
-    // Идём вправо.
     this.position += 1;
   }
 }
