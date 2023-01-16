4a186ddd631f2d89f764f30f4dceb4a443747403
Anastasia <anastasia-vinogradova5@yandex.ru>
2023-01-16 15:13:19 +0300

    added run

--- run.js
+++ run.js
@@ -1,12 +1,27 @@
 // Основной файл.
 // Запускает игру.
 const Game = require('./src/Game');
+const rl = require('readline');
 
-// Инициализация игры с настройками.
-const game = new Game({
-  trackLength: 30,
+const r = rl.createInterface({
+  input: process.stdin,
+  output: process.stdout,
 });
 
+r.question('Enter ur nickname:', function (answer) {
+  const game = new Game({
+    trackLength: 30,
+    name: answer,
+  });
+  console.log(answer);
+  console.log('Thank you for your valuable feedback.');
+  game.play();
+});
 
-// Запуск игры.
-game.play();
+// Инициализация игры с настройками.
+// const game = new Game({
+//   trackLength: 30,
+// });
+
+// // Запуск игры.
+// game.play();
