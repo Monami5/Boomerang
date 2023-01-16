a515e9ec0ead90fcdd03b1493e4b9d92080d7a84
eddy49reg <akhnazarov1990@gmail.com>
2023-01-16 14:39:07 +0300

    redact hero class

--- src/game-models/Hero.js
+++ src/game-models/Hero.js
@@ -1,33 +1,31 @@
 // Наш герой.
-const player = require('play-sound')((opts = {}));
+const Boomerang = require('./Boomerang');
 
 class Hero {
-  constructor({ position = 0 }) {
-    this.skin = '🦔'; // можете использовать любые emoji '💃'
+  constructor({ position, boomerang }) {
+    this.skin = '🦔';
     this.position = position;
+    this.boomerang = boomerang;
   }
 
   moveLeft() {
-    // Идём влево.
-    if (this.position > 0) this.position -= 1;
+    this.position -= 1;
   }
 
   moveRight() {
-    // Идём вправо.
-    if (this.position < 30) this.position += 1;
+    this.position += 1;
   }
 
   attack() {
-    // Атакуем.
     if (!this.boomerang.position) {
       this.boomerang.position = this.position + 1;
     }
-    player.play('src/sounds/game-over.wav');
-    this.skin = '👼';
-    console.log('RIP 👼 :(');
+  }
 
-    process.exit();
-    // this.boomerang.fly();
+  dieHero() {
+    this.skin = '💀';
+    console.log('YOU ARE DEAD!💀');
+    // process.exit();
   }
 }
 
